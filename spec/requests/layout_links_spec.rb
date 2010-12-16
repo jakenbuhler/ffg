2624353e2439e226dc05c5752f22870dd8dbf0f9
Jacob R. Rothenbuhler <jakerothenbuhler@gmail.com>
2010-12-16 15:44:18 -0800

    Added Test for Layout Links

--- spec/requests/layout_links_spec.rb
+++ spec/requests/layout_links_spec.rb
@@ -1,10 +1,19 @@
 require 'spec_helper'
 
 describe "LayoutLinks" do
+
   describe "GET /layout_links" do
+
     it "should have a Home page at '/'" do
       get '/'
       response.should have_selector('title', :content => "Home")
     end
+
+    it "should have the right links on the layout" do
+      visit root_path
+      click_link "Home"
+      response.should have_selector('title', :content => "Home")
+    end
+
   end
 end
