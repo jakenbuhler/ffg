7ba6d77bf426441fe97c7dc4a1b22e46bd55aabd
Jacob R. Rothenbuhler <jakerothenbuhler@gmail.com>
2010-12-19 16:47:55 -0800

    Done with User Sign In/Out

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -12,6 +12,7 @@ class UsersController < ApplicationController
   def create
     @user = User.new(params[:user])
     if @user.save
+      sign_in @user
       flash[:success] = "Welcome to the FFG! Please drink responsibly."
       redirect_to @user
     else
