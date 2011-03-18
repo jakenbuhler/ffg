1c9af8f43f8cd538885df9179882d61cecc8e0c7
Jacob R. Rothenbuhler <jakerothenbuhler@gmail.com>
2011-03-18 09:58:30 -0700

    Moved beers index link from admin to main navigation. (Closes GH-13)

--- spec/requests/layout_links_spec.rb
+++ spec/requests/layout_links_spec.rb
@@ -24,6 +24,8 @@ describe "LayoutLinks" do
     response.should have_selector("a", :href => signout_path)
     click_link "About"
     response.should have_selector('title', :content => "About")
+    click_link "Beers"
+    response.should have_selector('title', :content => "Beers")
     click_link "Profile"
     response.should have_selector('title', :content => @user.name)
     click_link "Admin"
