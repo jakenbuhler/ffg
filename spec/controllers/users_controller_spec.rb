bd8dcaf2dd960673270444c4f4ca14711e40a943
Jacob R. Rothenbuhler <jakerothenbuhler@gmail.com>
2011-03-18 11:21:41 -0700

    Removed automatic sign-in of newly created users.

--- spec/controllers/users_controller_spec.rb
+++ spec/controllers/users_controller_spec.rb
@@ -179,15 +179,10 @@ describe UsersController do
             post :create, :user => @attr
           end.should change(User, :count).by(1)
         end
-      
-        it "should sign the user in" do
-          post :create, :user => @attr
-          controller.should be_signed_in
-        end
 
         it "should redirect to the user show page" do
           post :create, :user => @attr
-          response.should redirect_to(user_path(assigns(:user)))
+          response.should redirect_to(users_path)
         end
       
         it "should have a welcome message" do
